7463373699ecf5de904641093d1ee634a143cfa4
Zøzo <benbar2412@gmail.com>
2024-06-11 22:42:38 +0200

    fix: architecture

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("./data/regional.json")
+    .then((response) => response.json())
+    .then((pokedexData) => {
+      const creatureList = document.getElementById("creature-list");
+
+      pokedexData.creatures.forEach((creature) => {
+        const creatureItem = document.createElement("div");
+        creatureItem.classList.add("creature-item");
+        creatureItem.innerHTML = `<strong>${creature.dbSymbol}</strong>: Form ${creature.form}`;
+        creatureList.appendChild(creatureItem);
+      });
+    })
+    .catch((error) => console.error("Error fetching the JSON data:", error));
+});
